890da569439db141c91130e65f45cc9ea94cbd54
Leeroy <leeroy@nowhere.com>
2025-09-19 16:06:55 +1000

    Wake lock on background scraping (screen off but scrapes)

--- src/org/leeroy/mediascraper/AutoScrapeService.java
+++ src/org/leeroy/mediascraper/AutoScrapeService.java
@@ -30,6 +30,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.IBinder;
+import android.os.PowerManager;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.ServiceCompat;
@@ -101,6 +102,8 @@ public class AutoScrapeService extends Service {
     private static Handler mHandler = new Handler(Looper.getMainLooper());
 
     private static Context mContext;
+    
+    private static PowerManager.WakeLock mWakeLock;
 
     private static final int NOTIFICATION_ID = 4;
     private NotificationManager nm;
@@ -136,6 +139,7 @@ public class AutoScrapeService extends Service {
     public static void startService(Context context) {
         log.debug("startService in foreground");
         mContext = context.getApplicationContext();
+        acquireWakeLock(context);
         Intent intent = new Intent(context, AutoScrapeService.class);
         try {
             // Try to start as foreground service, but fall back if not allowed
@@ -206,6 +210,13 @@ public class AutoScrapeService extends Service {
         synchronized (networkScanLock) {
             return networkScanCount;
         }
+    }
+    
+    public static void acquireWakeLock(Context context) {
+        //Keep screen on for scraping.
+        PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "LeeroyFlixScraper::WakeLock");
+        mWakeLock.acquire();
     }
 
     public void cleanup() {
@@ -859,6 +870,8 @@ public class AutoScrapeService extends Service {
     public void stopService() {
         log.debug("stopService");
         stopForeground(true);
+        if(mWakeLock!=null&&mWakeLock.isHeld())
+            mWakeLock.release();
     }
 
     private void saveDirtyState(boolean dirtyState) {
