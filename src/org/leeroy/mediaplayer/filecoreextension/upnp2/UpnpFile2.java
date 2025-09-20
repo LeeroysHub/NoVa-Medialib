86f35fc4d1b984b4466d7fbb7d42f160b68177ed
Leeroy <leeroy@nowhere.com>
2025-09-20 16:46:19 +1000

    HASH Collisions: getUniqueHash changes * Make number bigger, so shows don't collide * Change UPNP getUniqueHash to have string prefix. (Zero trim bug)

--- src/org/leeroy/mediaplayer/filecoreextension/upnp2/UpnpFile2.java
+++ src/org/leeroy/mediaplayer/filecoreextension/upnp2/UpnpFile2.java
@@ -175,8 +175,7 @@ public class UpnpFile2 extends MetaFile2 {
     }
 
     public String getUniqueHash(){
-        return String.format("%016x", getUri().getHost().hashCode()+length() +getName().hashCode());
-
+        return "LF" + String.format("%018x", Math.abs(getUri().hashCode()) + length() * Math.abs(getName().hashCode()));
     }
 
     @Override
