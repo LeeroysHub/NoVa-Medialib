d2891bc968b2d3dafd367c8e87555e6edde1066c
Leeroy <leeroy@nowhere.com>
2025-09-20 16:46:19 +1000

    getUniqueHash changes for scanning reliably?

--- src/org/leeroy/mediacenter/filecoreextension/HttpFile2.java
+++ src/org/leeroy/mediacenter/filecoreextension/HttpFile2.java
@@ -128,8 +128,7 @@ public class HttpFile2 extends MetaFile2 {
     }
 
     public String getUniqueHash(){
-        return String.format("%016x", getUri().getHost().hashCode()+length() +getName().hashCode());
-
+        return String.format("%016x", Math.abs(getUri().hashCode()) + length() * Math.abs(getName().hashCode()));
     }
 
     @Override
