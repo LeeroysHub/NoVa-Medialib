911f268367cf61747114c1554064cc284a6b8397
Leeroy <leeroy@nowhere.com>
2025-10-12 21:55:12 +1100

    Change Filename to default, not Name.

--- src/org/leeroy/mediascraper/xml/MovieScraper3.java
+++ src/org/leeroy/mediascraper/xml/MovieScraper3.java
@@ -101,9 +101,9 @@ public class MovieScraper3 extends BaseScraper2 {
         log.debug("movie search:{} year:{} language:{}", searchInfo.getName(), searchInfo.getYear(), language);
         
         //Check for UPNP and SMB differences, make sure we have a valid title.
-        String searchQuery = searchInfo.getName() != null ? searchInfo.getName() : searchInfo.getFile().toString();
+        String searchQuery = searchInfo.getFile() != null ? searchInfo.getFile().toString() : searchInfo.getName();
         if (searchQuery.toLowerCase().contains("null")) {
-            searchQuery = searchInfo.getFile().toString();
+            searchQuery = searchInfo.getName();
         }
         SearchMovieResult searchResult = SearchMovie2.search(searchQuery, language, searchInfo.getYear(), maxItems, searchService, adultScrape);
         // TODO: this triggers scrape for all search results, is this intended?
